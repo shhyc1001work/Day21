c980275bb0c6336ee0de094d4b96340c7ec4e378
SHIN_Han_Yeong <shhyc1001work@gmail.com>
2024-12-18 11:38:06 +0900

    [Feat] 기능 추가
    설명

--- Program.cs
+++ Program.cs
@@ -10,7 +10,9 @@ namespace Day21
             Console.WriteLine("이힣");
             Console.WriteLine("이힣");
             Console.WriteLine("이힣");
-
+            Console.WriteLine("기능개발 중");
+            Console.WriteLine("막 반복문도 적고, 클래스도 나누고");
+            Console.WriteLine("클래스 쪼개고, 기능 추가하고");
         }
     }
 }
